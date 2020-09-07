248be357e8c8e7b4e515d5612e88578d98674e8b
Omar <valis452@gmail.com>
2020-09-07 02:55:29 -0600

    Adding small tweaks and helper classes

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.22' );
+	define( '_S_VERSION', '1.0.4542' );
 }
 
 if ( ! function_exists( 'techgnosis_theme_setup' ) ) :
